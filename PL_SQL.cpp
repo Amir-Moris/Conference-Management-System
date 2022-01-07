51c757dcd99eeab5224da30c4624bd2cee329516
Maria801 <92376303+Maria801@users.noreply.github.com>
2022-01-07 23:55:23 +0200

    Update PL_SQL.cpp

--- PL_SQL.cpp
+++ PL_SQL.cpp
@@ -9,7 +9,8 @@
 --------------------------------------------------------
 --  DDL for Sequence EMPLOYEES_SEQ
 --------------------------------------------------------
-   CREATE SEQUENCE  "EMPLOYEES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 207 NOCACHE  NOORDER  NOCYCLE ;
+  //....
+CREATE SEQUENCE  "EMPLOYEES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 207 NOCACHE  NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence LOCATIONS_SEQ
 --------------------------------------------------------
